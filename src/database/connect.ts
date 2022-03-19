2c4c977cb57d28e31ed2efd3c9945a726eefd895
Yevgeny-Sh <yevgst7@gmail.com>
2022-03-19 17:35:17 +0200

    mongo bug

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 import * as path from 'path';
-console.log('qaaaa');
 
-console.log(config({ path: path.resolve(__dirname, '../mongo-atlas.env') }));
+//console.log(config({ path: path.resolve(__dirname, '../mongo-atlas.env') }));
 
-//config({ path: path.resolve(__dirname, '../mongo-atlas.env') });
+config({ path: path.resolve(__dirname, '../mongo-atlas.env') });
 
 export async function connectDb(): Promise<void> {
   try {
+    console.log(process.env.MONGO_URI);
+
     await mongoose.connect(`${process.env.MONGO_URI}`);
     const { host, name, port } = mongoose.connection;
     console.log('mongoose connection to atlas: ', { host, name, port });
